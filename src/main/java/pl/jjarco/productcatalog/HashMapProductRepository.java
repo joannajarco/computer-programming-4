51fa857a718107617006391164c22262cfa06aaa
joannajarco <asia-jarco@wp.pl>
2025-05-31 23:48:44 +0200

    class 09.04 adds springboot

---
+++ src/main/java/pl/jjarco/productcatalog/HashMapProductRepository.java
@@ -0,0 +1,21 @@
+package pl.jjarco.productcatalog;
+
+import java.util.List;
+
+public class HashMapProductRepository implements ProductRepository {
+
+    @Override
+    public List<Product> allProducts() {
+        return null;
+    }
+
+    @Override
+    public void save(Product newProduct) {
+
+    }
+
+    @Override
+    public Product loadProductById(String productId) {
+        return null;
+    }
+}
